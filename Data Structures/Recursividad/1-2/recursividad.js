b782b1c2ee1aea5e1af9e2b94e71fec556602006
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-11-01 15:02:30 -0600

    Reordening folders

---
+++ Data Structures/Recursividad/1-2/recursividad.js
@@ -0,0 +1,9 @@
+function imprimir(x){
+    
+    if(x > 0){
+        
+        imprimir(x - 1);
+        console.log(x)    
+        
+    }
+}
--- Recursividad/1-2/recursividad.js
+++
@@ -1,9 +0,0 @@
-function imprimir(x){
-    
-    if(x > 0){
-        
-        imprimir(x - 1);
-        console.log(x)    
-        
-    }
-}
